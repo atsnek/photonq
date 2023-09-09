702d49de5a6333e4a587a187a09cc0b3cf5c9e24
Jan-Emig <jan.emig@pm.me>
2023-09-09 22:58:05 +0200

    WIP: Move community posts to /user/posts/

---
+++ src/pages/user/posts/index.tsx
@@ -0,0 +1,11 @@
+import { PageConfig, PageProps } from '@atsnek/jaen';
+
+const Page: React.FC<PageProps> = () => {
+  return <>User Index</>;
+};
+
+export default Page;
+
+export const pageConfig: PageConfig = {
+  label: 'User'
+};
