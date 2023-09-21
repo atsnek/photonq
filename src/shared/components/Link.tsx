0941d193a71dad01008530b39d0e56ca130394af
Jan-Emig <jan.emig@pm.me>
2023-09-21 19:47:29 +0200

    feat(link): Added forwardRef feature

--- src/shared/components/Link.tsx
+++ src/shared/components/Link.tsx
@@ -1,6 +1,6 @@
 import { Link as ChLink, LinkProps, Text } from '@chakra-ui/react';
 import { Link as GaLink } from 'gatsby';
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, forwardRef } from 'react';
 import { isInternalLink } from '../utils/utils';
 import { useLocation } from '@reach/router';
 
@@ -13,16 +13,26 @@ interface GatsbyLinkProps extends LinkProps {
  * Custom link component combining Chakra UI's and Gatsby's Link.
  */
 
-const Link: FC<GatsbyLinkProps> = ({ href = '#', ...props }) => {
-  // Props that both link variants share.
-  const baseProps: LinkProps = {
-    position: 'relative'
-  };
+const Link = forwardRef<typeof ChLink, GatsbyLinkProps>(
+  ({ href = '#', ...props }, ref) => {
+    // Props that both link variants share.
+    const baseProps: LinkProps = {
+      position: 'relative'
+    };
 
-  if (isInternalLink(href)) {
-    return <ChLink {...baseProps} to={href} as={GaLink} {...props}></ChLink>;
+    if (isInternalLink(href)) {
+      return (
+        <ChLink
+          ref={ref}
+          {...baseProps}
+          to={href}
+          as={GaLink}
+          {...props}
+        ></ChLink>
+      );
+    }
+    return <ChLink ref={ref} {...baseProps} href={href} {...props}></ChLink>;
   }
-  return <ChLink {...baseProps} href={href} {...props}></ChLink>;
-};
+);
 
 export default Link;
