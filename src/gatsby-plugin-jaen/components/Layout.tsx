4be4d3b93d62cc39e8fac7fa858ec460524da69e
Nico Schett <nicoschett@icloud.com>
2023-09-24 13:21:09 +0200

    fix: profile and post title / create new post menu extension

--- src/gatsby-plugin-jaen/components/Layout.tsx
+++ src/gatsby-plugin-jaen/components/Layout.tsx
@@ -1,35 +1,35 @@
-import { LayoutProps } from '@atsnek/jaen';
-import AppLayout from '../../shared/containers/AppLayout';
-import { useLocation } from '@reach/router';
-import { useJaenFrameMenuContext } from 'gatsby-plugin-jaen';
-import { useEffect } from 'react';
-import { FaCogs } from 'react-icons/fa';
-import { useAppStore } from '../../shared/store/store';
-import { stat } from 'fs';
+import {LayoutProps} from '@atsnek/jaen'
+import AppLayout from '../../shared/containers/AppLayout'
+import {useLocation} from '@reach/router'
+import {useJaenFrameMenuContext} from 'gatsby-plugin-jaen'
+import {useEffect} from 'react'
+import {FaCogs} from 'react-icons/fa'
+import {useAppStore} from '../../shared/store/store'
+import {stat} from 'fs'
 
-const Layout: React.FC<LayoutProps> = ({ children, pageProps }) => {
-  const path = useLocation().pathname;
-  const hiddenTopNavPaths = ['/profile', '/blog-post'];
-  const fetchUser = useAppStore(state => state.currentUser.fetchUser);
+const Layout: React.FC<LayoutProps> = ({children, pageProps}) => {
+  const path = useLocation().pathname
+  const hiddenTopNavPaths = ['/profile', '/blog-post']
+  const fetchUser = useAppStore(state => state.currentUser.fetchUser)
 
-  const docsPaths = ['/docs', '/docs/community', '/docs/new-post/'];
+  const docsPaths = ['/docs', '/docs/community', '/docs/new-post/']
 
-  const jaenFrame = useJaenFrameMenuContext();
+  const jaenFrame = useJaenFrameMenuContext()
 
   useEffect(() => {
     jaenFrame.extendAddMenu({
       test: {
         label: 'New post',
         icon: FaCogs,
-        path: '/docs/new-post/'
+        path: '/docs/community/new-post/'
       }
-    });
+    })
 
-    fetchUser(); // Fetches the currently logged in user
-  }, []);
+    fetchUser() // Fetches the currently logged in user
+  }, [])
 
   if (path.startsWith('/admin') || path === '/') {
-    return children;
+    return children
   }
 
   return (
@@ -41,11 +41,10 @@ const Layout: React.FC<LayoutProps> = ({ children, pageProps }) => {
         isVisible: !hiddenTopNavPaths.some(hiddenPath =>
           path.startsWith(hiddenPath)
         )
-      }}
-    >
+      }}>
       {children}
     </AppLayout>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
