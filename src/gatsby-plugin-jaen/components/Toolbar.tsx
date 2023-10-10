d6115a1e00c9c0bf3e3ce59628507a44817a8022
Jan-Emig <jan.emig@pm.me>
2023-10-10 11:52:05 +0200

    feat(jaenToolbar): Change search input border color

--- src/gatsby-plugin-jaen/components/Toolbar.tsx
+++ src/gatsby-plugin-jaen/components/Toolbar.tsx
@@ -1,20 +1,21 @@
+import { useColorModeValue } from '@chakra-ui/react';
 import SearchMenu from '../../features/search/components/SearchMenu';
 
 export interface ToolbarProps {}
 
 export const Toolbar: React.FC<ToolbarProps> = () => {
+  const borderColor = useColorModeValue('gray.300', 'gray.700');
   return (
     <SearchMenu
       styleProps={{
         input: {
-          parent: { borderColor: 'gray.700', borderRadius: 'lg' },
+          parent: { borderColor: borderColor, borderRadius: 'lg' },
           kbd: { mt: 1, mr: 2 }
         },
         menuList: {
           width: { base: 0, md: '500px' },
           zIndex: 9999,
-          backgroundColor:
-            'var(--chakra-colors-features-search-menuList-bgColor)' // Otherwise, the color is not applied
+          backgroundColor: 'var(--chakra-colors-features-search-menuList-bgColor)' // Otherwise, the color is not applied
         }
       }}
     />
