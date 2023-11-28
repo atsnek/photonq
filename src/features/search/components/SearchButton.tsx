d9a9dcf5c64e591891a878d8195accd18975e6ab
Nico Schett <nicoschett@icloud.com>
2023-11-28 22:24:30 +0100

    feat(search): add responsive design to SearchButton component
    - Added imports for Box, IconButton, useBreakpointValue, and useMediaQuery from Chakra UI
    - Added conditional rendering based on the isMobile media query
    - Replaced span element with Box component for better accessibility

--- src/features/search/components/SearchButton.tsx
+++ src/features/search/components/SearchButton.tsx
@@ -1,6 +1,14 @@
-import { Button, Kbd } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  IconButton,
+  Kbd,
+  useBreakpointValue,
+  useMediaQuery
+} from '@chakra-ui/react';
 import { FC, useEffect, useState } from 'react';
 import { getPlatform } from '../../../shared/utils/utils';
+import { SearchIcon } from '@chakra-ui/icons';
 
 interface ISearchButtonProps {
   openModal: () => void;
@@ -12,6 +20,8 @@ interface ISearchButtonProps {
  */
 const SearchButton: FC<ISearchButtonProps> = ({ openModal, navigate }) => {
   const [kbd, setKbd] = useState<string | null>(null);
+
+  const [isMobile] = useMediaQuery('(max-width: 768px)'); // Adjust the breakpoint as needed
 
   useEffect(() => {
     setKbd(getPlatform() === 'mac' ? '⌘ K' : 'Ctrl+K');
@@ -26,6 +36,34 @@ const SearchButton: FC<ISearchButtonProps> = ({ openModal, navigate }) => {
       navigate(true);
     }
   };
+
+  if (isMobile) {
+    return (
+      <IconButton
+        size="sm"
+        variant="outline"
+        bgColor="blackAlpha.50"
+        color="topNav.input.color"
+        borderColor="topNav.input.borderColor"
+        fontWeight="normal"
+        icon={<SearchIcon />}
+        aria-label="Search documentation"
+        onClick={openModal}
+        onKeyDown={onKeyPress}
+      >
+        <Kbd
+          borderBottomWidth={1}
+          background="transparent"
+          borderRadius={4}
+          py={0.5}
+          ml={3}
+          opacity={0.7}
+        >
+          {kbd}
+        </Kbd>
+      </IconButton>
+    );
+  }
 
   return (
     <Button
@@ -50,7 +88,8 @@ const SearchButton: FC<ISearchButtonProps> = ({ openModal, navigate }) => {
       }}
       onClick={openModal}
     >
-      <span>Search documentation</span>
+      <Box as="span">Search documentation</Box>
+
       <Kbd
         borderBottomWidth={1}
         background="transparent"
