939c80027af639e1a171219b14cbe47ce665b347
Florian Kleber <kleber@snek.at>
2023-12-12 17:27:56 +0000

    feat(post): add skeleton loading to PostEditor

--- src/features/post/components/PostEditor.tsx
+++ src/features/post/components/PostEditor.tsx
@@ -1,9 +1,19 @@
-import { Dispatch, FC, RefObject, SetStateAction } from 'react';
-import { TPost } from '../types/post';
-import { Stack, Box, useBreakpointValue, Heading, Input, VStack } from '@chakra-ui/react';
-import UncontrolledMdxEditor from '../../../shared/components/mdx-editor/UncontrolledMdxEditor';
-import { MdastRoot } from '@atsnek/jaen-fields-mdx/dist/MdxField/components/types';
-import { useAppStore } from '../../../shared/store/store';
+import { Dispatch, FC, RefObject, SetStateAction } from "react";
+import { TPost } from "../types/post";
+import {
+  Stack,
+  Box,
+  useBreakpointValue,
+  Heading,
+  Input,
+  VStack,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react";
+import UncontrolledMdxEditor from "../../../shared/components/mdx-editor/UncontrolledMdxEditor";
+import { MdastRoot } from "@atsnek/jaen-fields-mdx/dist/MdxField/components/types";
+import { useAppStore } from "../../../shared/store/store";
+import { de } from "date-fns/locale";
 
 interface IPostEditorProps {
   titleRef: RefObject<HTMLInputElement>;
@@ -20,10 +30,16 @@ const PostEditor: FC<IPostEditorProps> = ({
   handleTitleChange,
   setIsSavingPost,
   madeChanges,
-  setMadeChanges
+  setMadeChanges,
 }) => {
-  const editorMargin = useBreakpointValue({ base: { mx: 5 }, xl: { mx: 0, mr: 5 } });
-  const updatePostContent = useAppStore(state => state.singlePost.editContent);
+  console.log(post);
+  const editorMargin = useBreakpointValue({
+    base: { mx: 5 },
+    xl: { mx: 0, mr: 5 },
+  });
+  const updatePostContent = useAppStore(
+    (state) => state.singlePost.editContent
+  );
   const showTitle = useBreakpointValue({ base: false, md: true });
 
   const handleEditorChange = async (value: MdastRoot) => {
@@ -34,6 +50,17 @@ const PostEditor: FC<IPostEditorProps> = ({
     if (!madeChanges) setMadeChanges(true);
   };
 
+  if (!post) {
+    // Render skeletons with the same layout as the editor
+
+    return (
+      <VStack w="full" gap={10}>
+        <Skeleton height="40px" w="full" />
+        <SkeletonText mt="4" noOfLines={8} spacing="4" w="full" />
+      </VStack>
+    );
+  }
+
   return (
     <VStack w="full" gap={10}>
       <Box
@@ -41,25 +68,25 @@ const PostEditor: FC<IPostEditorProps> = ({
         w="full"
         _before={{
           content: '""',
-          position: 'absolute',
+          position: "absolute",
           top: 0,
           bottom: 0,
-          my: 'auto',
-          w: '2px',
+          my: "auto",
+          w: "2px",
           h: 0,
-          bg: 'gray.300',
-          borderRadius: '2px',
-          transition: 'height 0.2s ease-in-out'
+          bg: "gray.300",
+          borderRadius: "2px",
+          transition: "height 0.2s ease-in-out",
         }}
         _hover={{
           _before: {
-            h: '50%'
-          }
+            h: "50%",
+          },
         }}
         _focusWithin={{
           _before: {
-            h: '80%'
-          }
+            h: "80%",
+          },
         }}
       >
         {showTitle && (
@@ -71,17 +98,23 @@ const PostEditor: FC<IPostEditorProps> = ({
             placeholder="My Post"
             fontSize="4xl"
             ml={2}
-            onChange={e => handleTitleChange(e.target.value)}
+            onChange={(e) => handleTitleChange(e.target.value)}
             defaultValue={post?.title}
           />
         )}
       </Box>
-      <Stack direction="row" position="relative" flex={1} overflow="hidden" w="full">
+      <Stack
+        direction="row"
+        position="relative"
+        flex={1}
+        overflow="hidden"
+        w="full"
+      >
         <Box w="full" {...editorMargin}>
           <UncontrolledMdxEditor
             onUpdateValue={handleEditorChange}
             isEditing
-            value={post?.content}
+            value={post.content}
           />
         </Box>
       </Stack>
