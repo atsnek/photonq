f5c529c9a31586d1c1f604b93f85834042b80a69
Nico Schett <nicoschett@icloud.com>
2024-04-19 17:14:48 +0200

    fix: UncontrolledMdxEditor
    - Use correct editor height
    - Use a minimised set of components called `experimentEditorComponents`

--- src/components/mdx-editor/UncontrolledMdxEditor.tsx
+++ src/components/mdx-editor/UncontrolledMdxEditor.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react';
-import { UncontrolledMdxField } from '@atsnek/jaen-fields-mdx';
+import { MdxFieldProps, UncontrolledMdxField } from '@atsnek/jaen-fields-mdx';
 import { mdxEditorComponents } from './MdxEditor';
 import Heading from '../main-content/heading/components/Heading';
 import { Stack } from '@chakra-ui/react';
+
+const experimentEditorComponents: MdxFieldProps['components'] = {
+  ...mdxEditorComponents
+};
+
+delete experimentEditorComponents.Filesystem;
+delete experimentEditorComponents.DocsIndex;
 
 interface IUncontrolledMdxEditorProps
   extends Omit<Parameters<typeof UncontrolledMdxField>[0], 'components'> {}
@@ -13,22 +20,28 @@ interface IUncontrolledMdxEditorProps
 const UncontrolledMdxEditor: FC<IUncontrolledMdxEditorProps> = ({
   ...props
 }) => {
-  console.log('UncontrolledMdxEditor', props);
-
   return (
-    <UncontrolledMdxField
-      {...props}
-      components={{
-        h1: props => <Heading variant="h1" {...props} noAnchor />,
-        h2: props => <Heading variant="h2" {...props} noAnchor />,
-        h3: props => <Heading variant="h3" {...props} noAnchor />,
-        h4: props => <Heading variant="h4" {...props} noAnchor />,
-        h5: props => <Heading variant="h5" {...props} noAnchor />,
-        h6: props => <Heading variant="h6" {...props} noAnchor />,
-        wrapper: ({ children }) => <Stack>{children}</Stack>,
-        ...mdxEditorComponents
+    <Stack
+      sx={{
+        '.cm-editor': {
+          height: '60dvh'
+        }
       }}
-    />
+    >
+      <UncontrolledMdxField
+        {...props}
+        components={{
+          h1: props => <Heading variant="h1" {...props} noAnchor />,
+          h2: props => <Heading variant="h2" {...props} noAnchor />,
+          h3: props => <Heading variant="h3" {...props} noAnchor />,
+          h4: props => <Heading variant="h4" {...props} noAnchor />,
+          h5: props => <Heading variant="h5" {...props} noAnchor />,
+          h6: props => <Heading variant="h6" {...props} noAnchor />,
+          wrapper: ({ children }) => <Stack>{children}</Stack>,
+          ...experimentEditorComponents
+        }}
+      />
+    </Stack>
   );
 };
 
