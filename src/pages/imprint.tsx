11b08200bd2818f5cb250d474ae6712a2e86c265
Nico Schett <nicoschett@icloud.com>
2024-04-20 19:10:00 +0200

    feat: add imprint

---
+++ src/pages/imprint.tsx
@@ -0,0 +1,36 @@
+import { Field, PageConfig, PageProps } from '@atsnek/jaen';
+
+import { Box } from '@chakra-ui/react';
+import { graphql } from 'gatsby';
+import * as React from 'react';
+
+const ImprintPage: React.FC<PageProps> = () => {
+  return (
+    <Box as="main">
+      <Field.Editor name="imprint" />
+    </Box>
+  );
+};
+
+export default ImprintPage;
+
+export const pageConfig: PageConfig = {
+  label: 'Imprint page',
+  icon: 'FaPassport'
+};
+
+export const query = graphql`
+  query ($jaenPageId: String!) {
+    ...JaenPageQuery
+    allJaenPage {
+      nodes {
+        ...JaenPageData
+        children {
+          ...JaenPageData
+        }
+      }
+    }
+  }
+`;
+
+export { Head } from '@atsnek/jaen';
