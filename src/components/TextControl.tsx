1413fe6487476acdff2d36568b0eb35195a106fc
Nico Schett <nicoschett@icloud.com>
2024-05-16 13:48:53 +0200

    feat: update TextControl styling

--- src/components/TextControl.tsx
+++ src/components/TextControl.tsx
@@ -60,19 +60,24 @@ export const TextControl: React.FC<{
   return (
     <Editable
       as={HStack}
-      textAlign={props.type === 'heading' ? 'center' : 'left'}
+      textAlign={'left'}
       isPreviewFocusable={props.editable ? true : false}
       defaultValue={props.text}
       onSubmit={props.onSubmit}
-      fontSize={props.type === 'heading' ? '4xl' : undefined}
-      fontWeight={props.type === 'heading' ? 'bold' : 'normal'}
+      fontSize={props.type === 'heading' ? '2xl' : 'xl'}
+      fontWeight={props.type === 'heading' ? 'bold' : '400'}
     >
       <EditablePreview as={props.type === 'heading' ? Heading : Text} />
       {/* Here is the custom input */}
       {props.type === 'heading' ? (
-        <Input as={EditableInput} />
+        <Input as={EditableInput} fontSize="2xl" fontWeight="bold" />
       ) : (
-        <Textarea as={EditableTextarea} />
+        <Textarea
+          as={EditableTextarea}
+          fontSize="xl"
+          color="gray.600"
+          fontWeight="400"
+        />
       )}
       {props.editable && <EditableControls />}
     </Editable>
