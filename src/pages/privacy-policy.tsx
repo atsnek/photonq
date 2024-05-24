0cfd7ccb0af9fe492573c0525768bd33e2cf7ad0
Florian Kleber <kleber@snek.at>
2024-05-24 09:37:59 +0000

    feat(pages): add privacy-policy page with editor
    Adds a new Privacy Policy page to the project using 'Field.Editor' for editable content. The page is configured with a label and an icon.

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,36 @@
+import { Field, PageConfig, PageProps } from '@atsnek/jaen';
+
+import { Box } from '@chakra-ui/react';
+import { graphql } from 'gatsby';
+import * as React from 'react';
+
+const PrivacyPolicyPage: React.FC<PageProps> = () => {
+  return (
+    <Box as="main">
+      <Field.Editor name="privacy-policy" />
+    </Box>
+  );
+};
+
+export default PrivacyPolicyPage;
+
+export const pageConfig: PageConfig = {
+  label: 'PrivacyPolicy page',
+  icon: 'FaPassport'
+};
+
+export const query = graphql`
+  query ($jaenPageId: String!) {
+    ...JaenPageQuery
+    allJaenPage {
+      nodes {
+        ...JaenPageData
+        children {
+          ...JaenPageData
+        }
+      }
+    }
+  }
+`;
+
+export { Head } from '@atsnek/jaen';
