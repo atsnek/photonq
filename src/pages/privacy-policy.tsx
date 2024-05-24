3d140195083defc5140a0be16a0ec447c25a4826
Nico Schett <nicoschett@icloud.com>
2024-05-24 15:13:22 +0200

    feat: add privacy-policy

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,29 @@
+import { PageConfig, PageProps } from '@atsnek/jaen';
+
+import { graphql, navigate } from 'gatsby';
+import * as React from 'react';
+
+const Page: React.FC<PageProps> = () => {
+  React.useEffect(() => {
+    navigate(
+      'https://dsba.univie.ac.at/fileadmin/user_upload/p_dsba/datenschutzerklaerung_websites_V04_26062020_EN.pdf'
+    );
+  }, []);
+
+  return null;
+};
+
+export default Page;
+
+export const pageConfig: PageConfig = {
+  label: 'Privacy Policy',
+  icon: 'FaPassport'
+};
+
+export const query = graphql`
+  query ($jaenPageId: String!) {
+    ...JaenPageQuery
+  }
+`;
+
+export { Head } from '@atsnek/jaen';
