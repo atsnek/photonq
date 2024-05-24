835f89ebd16b5a4026798f082514c58de013604b
Florian Kleber <kleber@snek.at>
2024-05-24 12:03:53 +0000

    feat(gatsby-plugin-jaen): redirect privacy-policy page to external PDF
    Move privacy policy redirect logic from gatsby-browser to Layout component. Redirect to external PDF when accessing the privacy-policy page.

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,11 +1,1 @@
 import './src/styles/global.css';
-
-exports.onRouteUpdate = ({ location }) => {
-  // Check if the current route is the privacy policy page
-
-  if (location.pathname === '/privacy-policy') {
-    window.location.replace(
-      'https://dsba.univie.ac.at/fileadmin/user_upload/p_dsba/datenschutzerklaerung_websites_V04_26062020_EN.pdf'
-    );
-  }
-};
