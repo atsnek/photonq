9246a87b8185b58b7e9d07809eb05f748d6f7826
Florian Kleber <kleber@snek.at>
2024-05-24 09:39:06 +0000

    feat(pages): add terms-of-service page with editor
    Adds a new Terms of Service page to the project using 'Field.Editor' for editable content. The page is configured with a label and an icon.

---
+++ src/pages/terms-of-service.tsx
@@ -0,0 +1,36 @@
+import { Field, PageConfig, PageProps } from '@atsnek/jaen';
+
+import { Box } from '@chakra-ui/react';
+import { graphql } from 'gatsby';
+import * as React from 'react';
+
+const TermsOfServicePage: React.FC<PageProps> = () => {
+  return (
+    <Box as="main">
+      <Field.Editor name="terms-of-service" />
+    </Box>
+  );
+};
+
+export default TermsOfServicePage;
+
+export const pageConfig: PageConfig = {
+  label: 'TermsOfServicePage page',
+  icon: 'FaPassport'
+};
+
+export const query = graphql`
+  query ($jaenPageId: String!) {
+    ...JaenPageQuery
+    allJaenPage {
+      nodes {
+        ...JaenPageData
+        children {
+          ...JaenPageData
+        }
+      }
+    }
+  }
+`;
+
+export { Head } from '@atsnek/jaen';
